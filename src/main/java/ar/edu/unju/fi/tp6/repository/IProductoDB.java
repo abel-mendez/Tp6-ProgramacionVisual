48eb527e35e05e150afc1c5158d44a33f9a50221
abel-mendez <abel.m.villagra@gmail.com>
2021-05-20 19:48:41 -0300

    corrigiendo lo adelantado...

--- src/main/java/ar/edu/unju/fi/tp6/repository/IProductoDB.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.unju.fi.tp6.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ar.edu.unju.fi.tp6.model.Producto;
-
-public interface IProductoDB extends JpaRepository<Producto, Integer > {
-	
-}
